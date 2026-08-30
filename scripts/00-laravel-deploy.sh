0136b59a0ad85e67913649f16bd6c6201b9b5e51
Dhann8 <imdhan26@gmail.com>
2026-08-30 19:46:34 +0700

    feat: add npm install and build to deploy script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
+# Install PHP Dependencies
 composer install --no-dev --working-dir=/var/www/html
+
+# Install NPM Dependencies & Build Assets (CSS/JS)
+npm install
+npm run build
+
+# Clear & Cache Laravel Configurations
 php artisan config:cache
 php artisan route:cache
+php artisan view:cache
+
+# Run Migrations & Seeders
 php artisan migrate --force
 php artisan db:seed --force
