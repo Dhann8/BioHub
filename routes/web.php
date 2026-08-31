d7ed9f74acd1bd4bf7175ff8d7f5b073a1cf5ffb
Dhann8 <imdhan26@gmail.com>
2026-08-31 11:10:35 +0700

    Fix duplicate fauna destroy route

--- routes/web.php
+++ routes/web.php
@@ -168,7 +168,6 @@ Route::middleware('auth')->group(function () {
         Route::post('/contributions/{id}/approve', [ContributionController::class, 'approveContribution'])->name('contributions.approve');
         Route::post('/contributions/{id}/reject', [ContributionController::class, 'rejectContribution'])->name('contributions.reject');
         Route::get('/crowdsourcing', [ContributionController::class, 'adminIndex'])->name('crowdsourcing.index');
-        Route::delete('/fauna/{id}', [FaunaController::class, 'destroy'])->name('fauna.destroy');
         Route::get('/settings', [\App\Http\Controllers\SettingController::class, 'index'])->name('settings');
         Route::post('/settings', [\App\Http\Controllers\SettingController::class, 'update'])->name('settings.update');
     });
